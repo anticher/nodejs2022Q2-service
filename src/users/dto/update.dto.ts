43e137ac6d0b9a3f299f851fc7e9b7b96aadf8b0
anticher <anticherq@gmail.com>
2022-07-13 20:20:02 +0300

    feat: add users status codes

--- src/users/dto/update.dto.ts
+++ src/users/dto/update.dto.ts
@@ -1,4 +1,9 @@
-export interface UpdatePasswordDto {
+import { IsString } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString()
   oldPassword: string;
+
+  @IsString()
   newPassword: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create.dto';
 import { User } from './schemas/user.schema';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate as isValidUUID } from 'uuid';
 import { UpdatePasswordDto } from './dto/update.dto';
 
 @Injectable()
@@ -10,40 +10,56 @@ export class UsersService {
 
   create(createUserDto: CreateUserDto): User {
     const user: User = createUserDto;
+    if (!user.login || !user.password) {
+      throw new HttpException(
+        'user does not contain required fields: login and password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     user.id = uuidv4();
     this.list.push(user);
     return user;
   }
 
   update(id: string, updatePasswordDto: UpdatePasswordDto): User | undefined {
-    const user = this.list.find((item) => item.id === id);
-    if (user.password === updatePasswordDto.oldPassword) {
-      user.password = updatePasswordDto.newPassword;
-      return user;
+    if (!isValidUUID(id)) {
+      throw new HttpException('userId is invalid', HttpStatus.BAD_REQUEST);
     }
+    const user = this.list.find((item) => item.id === id);
+    if (!user) {
+      throw new HttpException('user does not exist', HttpStatus.NOT_FOUND);
+    }
+    if (user.password !== updatePasswordDto.oldPassword) {
+      throw new HttpException('oldPassowrd is wrong', HttpStatus.FORBIDDEN);
+    }
+    user.password = updatePasswordDto.newPassword;
+    return user;
   }
 
   delete(id: string): void {
+    if (!isValidUUID(id)) {
+      throw new HttpException('userId is invalid', HttpStatus.BAD_REQUEST);
+    }
     const itemIndex = this.list.findIndex((item) => item.id === id);
+    if (itemIndex < 0) {
+      throw new HttpException('user does not exist', HttpStatus.NOT_FOUND);
+    }
     this.list.splice(itemIndex, 1);
-    return;
+    throw new HttpException('', HttpStatus.NO_CONTENT);
   }
 
   getUser(id: string): User {
-    const result = this.list.find((item) => item.id === id);
-    return result;
+    if (!isValidUUID(id)) {
+      throw new HttpException('userId is invalid', HttpStatus.BAD_REQUEST);
+    }
+    const user = this.list.find((item) => item.id === id);
+    if (!user) {
+      throw new HttpException('user does not exist', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 
   getAll(): User[] {
     return this.list;
   }
-
-  //   GET /user/:id - get single user by id
-  // Server should answer with status code 200 and and record with id === userId if it exists
-  // Server should answer with status code 400 and corresponding message if userId is invalid (not uuid)
-  // Server should answer with status code 404 and corresponding message if record with id === userId doesn't exist
-
-  // async findOneById(id): Promise<User> {
-  //     return this.userModel.findById(id).exec();
-  // }
 }
