6ab8ad407a5e9ef0297710eb64cd0c84027e1a26
anticher <anticherq@gmail.com>
2022-07-17 18:03:02 +0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
     .setDescription('Home music library service')
