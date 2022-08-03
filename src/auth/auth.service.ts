fd7a92a5420242fcf58bb887c98fb16f9cb8e9ea
anticher <anticherq@gmail.com>
2022-08-03 21:54:46 +0300

    feat: add bcryptjs password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create.dto';
 import { User } from 'src/users/models/user.model';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,12 +13,18 @@ export class AuthService {
   ) {}
 
   async signup(user: CreateUserDto) {
+    const saltOrRounds = 10;
+    user.password = await bcrypt.hash(user.password, saltOrRounds);
     return this.usersService.create(user);
   }
 
   async login(inputData: CreateUserDto) {
     const user = await this.usersService.getUserByLogin(inputData.login);
-    if (!user || user.password !== inputData.password) {
+    const isPasswordsMatch = await bcrypt.compare(
+      inputData.password,
+      user.password,
+    );
+    if (!user || !isPasswordsMatch) {
       throw new HttpException(
         'no user with such login or password does not match actual one',
         HttpStatus.FORBIDDEN,
